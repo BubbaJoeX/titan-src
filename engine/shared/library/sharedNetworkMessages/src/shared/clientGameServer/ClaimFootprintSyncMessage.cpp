040f048fbb86367ae56911e8059892717b95c3ca
BubbaJoeX <jojojojo8@live.com>
2026-05-17 19:26:08 -0500

    Claims

--- engine/shared/library/sharedNetworkMessages/src/shared/clientGameServer/ClaimFootprintSyncMessage.cpp
+++ engine/shared/library/sharedNetworkMessages/src/shared/clientGameServer/ClaimFootprintSyncMessage.cpp
@@ -1,87 +1,87 @@
-// ======================================================================
-//
-// ClaimFootprintSyncMessage.cpp
-//
-// ======================================================================
-
-#include "sharedNetworkMessages/FirstSharedNetworkMessages.h"
-#include "sharedNetworkMessages/ClaimFootprintSyncMessage.h"
-
-// ======================================================================
-
-char const * const ClaimFootprintSyncMessage::cms_name = "ClaimFootprintSyncMessage";
-
-// ======================================================================
-
-ClaimFootprintSyncMessage::ClaimFootprintSyncMessage(NetworkId const & markerId, Vector const & center, float const radiusMeters, bool const active) :
-	GameNetworkMessage(cms_name),
-	m_markerId(markerId),
-	m_centerX(center.x),
-	m_centerY(center.y),
-	m_centerZ(center.z),
-	m_radiusMeters(radiusMeters),
-	m_active(active)
-{
-	addVariable(m_markerId);
-	addVariable(m_centerX);
-	addVariable(m_centerY);
-	addVariable(m_centerZ);
-	addVariable(m_radiusMeters);
-	addVariable(m_active);
-}
-
-// ----------------------------------------------------------------------
-
-ClaimFootprintSyncMessage::ClaimFootprintSyncMessage(Archive::ReadIterator & source) :
-	GameNetworkMessage(cms_name),
-	m_markerId(),
-	m_centerX(0.f),
-	m_centerY(0.f),
-	m_centerZ(0.f),
-	m_radiusMeters(0.f),
-	m_active(false)
-{
-	addVariable(m_markerId);
-	addVariable(m_centerX);
-	addVariable(m_centerY);
-	addVariable(m_centerZ);
-	addVariable(m_radiusMeters);
-	addVariable(m_active);
-	AutoByteStream::unpack(source);
-}
-
-// ----------------------------------------------------------------------
-
-ClaimFootprintSyncMessage::~ClaimFootprintSyncMessage()
-{
-}
-
-// ----------------------------------------------------------------------
-
-NetworkId const & ClaimFootprintSyncMessage::getMarkerId() const
-{
-	return m_markerId.get();
-}
-
-// ----------------------------------------------------------------------
-
-Vector const ClaimFootprintSyncMessage::getCenter() const
-{
-	return Vector(m_centerX.get(), m_centerY.get(), m_centerZ.get());
-}
-
-// ----------------------------------------------------------------------
-
-float ClaimFootprintSyncMessage::getRadiusMeters() const
-{
-	return m_radiusMeters.get();
-}
-
-// ----------------------------------------------------------------------
-
-bool ClaimFootprintSyncMessage::getActive() const
-{
-	return m_active.get();
-}
-
-// ======================================================================
+// ======================================================================
+//
+// ClaimFootprintSyncMessage.cpp
+//
+// ======================================================================
+
+#include "sharedNetworkMessages/FirstSharedNetworkMessages.h"
+#include "sharedNetworkMessages/ClaimFootprintSyncMessage.h"
+
+// ======================================================================
+
+char const * const ClaimFootprintSyncMessage::cms_name = "ClaimFootprintSyncMessage";
+
+// ======================================================================
+
+ClaimFootprintSyncMessage::ClaimFootprintSyncMessage(NetworkId const & markerId, Vector const & center, float const radiusMeters, bool const active) :
+	GameNetworkMessage(cms_name),
+	m_markerId(markerId),
+	m_centerX(center.x),
+	m_centerY(center.y),
+	m_centerZ(center.z),
+	m_radiusMeters(radiusMeters),
+	m_active(active)
+{
+	addVariable(m_markerId);
+	addVariable(m_centerX);
+	addVariable(m_centerY);
+	addVariable(m_centerZ);
+	addVariable(m_radiusMeters);
+	addVariable(m_active);
+}
+
+// ----------------------------------------------------------------------
+
+ClaimFootprintSyncMessage::ClaimFootprintSyncMessage(Archive::ReadIterator & source) :
+	GameNetworkMessage(cms_name),
+	m_markerId(),
+	m_centerX(0.f),
+	m_centerY(0.f),
+	m_centerZ(0.f),
+	m_radiusMeters(0.f),
+	m_active(false)
+{
+	addVariable(m_markerId);
+	addVariable(m_centerX);
+	addVariable(m_centerY);
+	addVariable(m_centerZ);
+	addVariable(m_radiusMeters);
+	addVariable(m_active);
+	AutoByteStream::unpack(source);
+}
+
+// ----------------------------------------------------------------------
+
+ClaimFootprintSyncMessage::~ClaimFootprintSyncMessage()
+{
+}
+
+// ----------------------------------------------------------------------
+
+NetworkId const & ClaimFootprintSyncMessage::getMarkerId() const
+{
+	return m_markerId.get();
+}
+
+// ----------------------------------------------------------------------
+
+Vector ClaimFootprintSyncMessage::getCenter() const
+{
+	return Vector(m_centerX.get(), m_centerY.get(), m_centerZ.get());
+}
+
+// ----------------------------------------------------------------------
+
+float ClaimFootprintSyncMessage::getRadiusMeters() const
+{
+	return m_radiusMeters.get();
+}
+
+// ----------------------------------------------------------------------
+
+bool ClaimFootprintSyncMessage::getActive() const
+{
+	return m_active.get();
+}
+
+// ======================================================================
--- engine/shared/library/sharedNetworkMessages/src/shared/clientGameServer/ClaimFootprintSyncMessage.h
+++ engine/shared/library/sharedNetworkMessages/src/shared/clientGameServer/ClaimFootprintSyncMessage.h
@@ -1,48 +1,48 @@
-// ======================================================================
-//
-// ClaimFootprintSyncMessage.h
-//
-// Syncs open-world claim boundary visuals (pylon + ribbon) to clients.
-//
-// ======================================================================
-
-#ifndef INCLUDED_ClaimFootprintSyncMessage_H
-#define INCLUDED_ClaimFootprintSyncMessage_H
-
-// ======================================================================
-
-#include "sharedFoundation/NetworkId.h"
-#include "sharedMath/Vector.h"
-#include "sharedNetworkMessages/GameNetworkMessage.h"
-
-// ======================================================================
-
-class ClaimFootprintSyncMessage : public GameNetworkMessage
-{
-public:
-	static char const * const cms_name;
-
-	ClaimFootprintSyncMessage(NetworkId const & markerId, Vector const & center, float radiusMeters, bool active);
-	explicit ClaimFootprintSyncMessage(Archive::ReadIterator & source);
-	~ClaimFootprintSyncMessage();
-
-	NetworkId const & getMarkerId() const;
-	Vector const &      getCenter() const;
-	float               getRadiusMeters() const;
-	bool                getActive() const;
-
-private:
-	Archive::AutoVariable<NetworkId> m_markerId;
-	Archive::AutoVariable<float>     m_centerX;
-	Archive::AutoVariable<float>     m_centerY;
-	Archive::AutoVariable<float>     m_centerZ;
-	Archive::AutoVariable<float>     m_radiusMeters;
-	Archive::AutoVariable<bool>      m_active;
-
-	ClaimFootprintSyncMessage(ClaimFootprintSyncMessage const &);
-	ClaimFootprintSyncMessage & operator=(ClaimFootprintSyncMessage const &);
-};
-
-// ======================================================================
-
-#endif
+// ======================================================================
+//
+// ClaimFootprintSyncMessage.h
+//
+// Syncs open-world claim boundary visuals (pylon + ribbon) to clients.
+//
+// ======================================================================
+
+#ifndef INCLUDED_ClaimFootprintSyncMessage_H
+#define INCLUDED_ClaimFootprintSyncMessage_H
+
+// ======================================================================
+
+#include "sharedFoundation/NetworkId.h"
+#include "sharedMath/Vector.h"
+#include "sharedNetworkMessages/GameNetworkMessage.h"
+
+// ======================================================================
+
+class ClaimFootprintSyncMessage : public GameNetworkMessage
+{
+public:
+	static char const * const cms_name;
+
+	ClaimFootprintSyncMessage(NetworkId const & markerId, Vector const & center, float radiusMeters, bool active);
+	explicit ClaimFootprintSyncMessage(Archive::ReadIterator & source);
+	~ClaimFootprintSyncMessage();
+
+	NetworkId const & getMarkerId() const;
+	Vector              getCenter() const;
+	float               getRadiusMeters() const;
+	bool                getActive() const;
+
+private:
+	Archive::AutoVariable<NetworkId> m_markerId;
+	Archive::AutoVariable<float>     m_centerX;
+	Archive::AutoVariable<float>     m_centerY;
+	Archive::AutoVariable<float>     m_centerZ;
+	Archive::AutoVariable<float>     m_radiusMeters;
+	Archive::AutoVariable<bool>      m_active;
+
+	ClaimFootprintSyncMessage(ClaimFootprintSyncMessage const &);
+	ClaimFootprintSyncMessage & operator=(ClaimFootprintSyncMessage const &);
+};
+
+// ======================================================================
+
+#endif
